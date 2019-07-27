38b5ab7a855616c97937917e0966b05ae1d1397b
Aaron Huang <chlbqj@gmail.com>
2019-07-28 02:40:00 +0800

    player頁面球員基本資料一半

--- nbs/src/views/Player.vue
+++ nbs/src/views/Player.vue
@@ -1,7 +1,31 @@
 <template>
-	<div class="container my-3">
-		<div>
-			<b-card-group deck>
+	<div>
+		<div class="my-1 bg-dark text-white">
+			<div class="container mx-3">
+				<div class="row justify-content-start align-items-center">
+					<div class="col-auto">
+						<img src="../assets/jamesHarden.png" />
+					</div>
+					<div class="col-auto">
+						<div class="row justify-content-start align-items-end pt-2 mb-1">
+							<h1 class="col-auto font-italic" id="number">#13</h1>
+							<div class="col-auto px-0">
+								<p class="name">James</p>
+								<p class="name">Harden</p>
+							</div>
+						</div>
+						<div class="row justify-content-start mx-0">
+							<img id="logo" class="mr-2" src="../assets/basketball.svg" />
+							<span>G</span>
+							<div id="vl" class="mx-2 pb-0"></div>
+							<span>Houston Rocket</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container my-3">
+			<div>
 				<b-card
 					img-src="http://lorempixel.com/250/300/sports"
 					blank-color="grey"
@@ -18,14 +42,16 @@
 						:visual-map="visualMap"
 						:judge-width="true"
 						:data="chartData"
-						v-if="chartData"
 						:legend="legendSetting"
+						:settings="chartSettings"
+						v-if="chartData"
 					></ve-line>
-					<div v-else>
+
+					<div class="d-flex justify-content-center flex-wrap my-5" v-else>
 						<b-spinner variant="primary" label="Text Centered"></b-spinner>
 					</div>
 				</b-card>
-			</b-card-group>
+			</div>
 		</div>
 	</div>
 </template>
@@ -35,15 +61,14 @@
 		components: {},
 		data() {
 			return {
+				chartSettings: {
+					area: true,
+					scale: [true, true] //基本值是否為0，true為否
+				},
 				legendSetting: {
 					textStyle: {
 						color: "white",
 						fontSize: 14
-					}
-				},
-				titleSetting: {
-					textStyle: {
-						color: "white"
 					}
 				},
 				textStyles: {
@@ -60,7 +85,10 @@
 						top: "50%",
 						precision: 1,
 						inRange: {
-							color: ['#333333', '#777777']
+							color: ["#63aabc", "#daf1f9", "#fb9935", "#b90b0b"]
+						},
+						textStyle: {
+							color: "white"
 						}
 					}
 				],
@@ -79,14 +107,14 @@
 					this.chartData = {
 						columns: ["season", "points"],
 						rows: [
-							{ season: "2012-13", points: 19.0 },
+							{ season: "2012-13", points: 5.0 },
 							{ season: "2013-14", points: 20.7 },
 							{ season: "2014-15", points: 21.0 },
 							{ season: "2015-16", points: 25.1 },
 							{ season: "2016-15", points: 27.0 }
 						]
 					};
-				}, 2000);
+				}, 1500);
 			}
 		}
 	};
@@ -95,5 +123,31 @@
 <style scoped lang="scss">
 	.card-body {
 		padding: 0px;
+	}
+
+	.name {
+		line-height: 0.3rem;
+		font-size: 1.2rem;
+		letter-spacing: 1px;
+		font-weight: bold;
+	}
+
+	#number {
+		font-size: 3rem;
+		line-height: 2.5rem;
+	}
+
+	#vl {
+		border-left: 2px solid white;
+		height: 1rem;
+	}
+
+	span {
+		line-height: 1rem;
+	}
+
+	#logo {
+		height: 1rem;
+		width: auto;
 	}
 </style>
