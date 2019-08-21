2327d78af3e63ea9ccde80bcfc0bb9ccd14ba232
willie_chen <finish331520@gmail.com>
2019-08-21 22:20:49 +0800

    firebase環境建置

--- nbs/src/main.js
+++ nbs/src/main.js
@@ -6,15 +6,17 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import ElementUI from 'element-ui'
+import VueFirestore from 'vue-firestore'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'vue-loading-overlay/dist/vue-loading.css'
 import vuetify from './plugins/vuetify';
 import '@/assets/scss/global.scss'
+import 'firebase/firestore'
 
 Vue.use(BootstrapVue)
 Vue.use(VCharts)
 Vue.use(ElementUI)
-
+Vue.use(VueFirestore)
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
