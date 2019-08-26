9a667fe9a92f68541027c88750caee1a2d62d446
Aaron Huang <chlbqj@gmail.com>
2019-08-26 21:09:42 +0800

    爬蟲掛掉

--- python/nbs/spiders/playerCrawler.py
+++ python/nbs/spiders/playerCrawler.py
@@ -2,12 +2,13 @@
 # 輸入scrapy startproject 'projectName' 創建專案
 # 輸入scrapy crawl 'fileName'執行
 # 輸入scrapy crawl 'fileName' -o fileName.檔案類別 -t 檔案類別 ; -o代表等一下要輸出的檔案 -t是要用的格式
+#scrapy crawl 'filename' --logfile='logFileName.log' 將log存入文字檔
 
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from bs4 import BeautifulSoup as BS
-from nbs.items import NbsItem
+from NBS.items import NbsItem
 import re
 import pandas
 
@@ -15,13 +16,12 @@ import pandas
 class PlayerCrawler(CrawlSpider):
     name = 'player'
     domain = 'https://www.basketball-reference.com/players/'
-    start_urls = []
-    for alphabet in range(97, 123):
-        start_urls.append(domain + chr(alphabet) + '/')
+    start_urls = ['https://www.basketball-reference.com/players/a/']
+    # for alphabet in range(97, 123):
+    #     start_urls.append(domain + chr(alphabet) + '/')
 
     rules = [
-        Rule(LinkExtractor(allow=(
-            'https://www.basketball-reference.com/players/[a-z]')), callback='parse', follow='true')
+        Rule(LinkExtractor(allow=('https://www.basketball-reference.com/players/[a-z]')), callback='parse', follow='true')
     ]
 
     def parse(self, response):
@@ -31,16 +31,18 @@ class PlayerCrawler(CrawlSpider):
         for index in table.select('tr'):
             index = index.select('th')[0]
             url = index.select('a')[0].get('href')
-            yield scrapy.Request(domain + url, self.parse_detail)
+            yield scrapy.Request(domain + url, self.parse_detail)   #進入各個球員頁面
 
     def parse_detail(self, response):
         team = None
         res = BS(response.body, 'lxml')
-        table = pandas.read_html(response.url)[0]
-        name = res.select('h1')
-        number = res.select('text')
-        meta = res.select('#meta')[0]
-        meta = meta.select('p')
+        table = pandas.read_html(response.url)[0]   #球員歷年數據表格
+        name = res.select('h1')     #球員名字
+        number = res.select('text')     #球員最後一個背號
+        birth = res.select('#necro-birth')[0]
+        birth_year = birth.select('a')[1]   #球員出生年
+        meta = res.select('#meta')[0]   #球員基本資料區
+        meta = meta.select('p')     #球員基本資料
 
         for strongStr in meta:
             if len(strongStr.select('strong')) != 0:
@@ -49,17 +51,21 @@ class PlayerCrawler(CrawlSpider):
                 if re.findall("Team", strongStr.select('strong')[0].text):
                     team = strongStr.select('a')[0].text
 
-        str = re.split("\n", position)
+        #取得球員位置
+        str = re.split('\n', position)
         for i in range(len(str)):
             if str[i] == '  Position:':
                 break
         position = str[i+2][2:]
 
+        #存成Json檔
         nbsItems = NbsItem()
+        # nbsItems['id'] = id
+        nbsItems['birth'] = birth_year.text
         nbsItems['name'] = name[0].text
         nbsItems['number'] = number[len(number)-1].text
         if team == None:
-            nbsItems['team'] = None
+            nbsItems['team'] = None     #如果目前沒有球隊則為null
         else:
             nbsItems['team'] = team
         nbsItems['position'] = position
