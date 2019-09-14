cbfae429a09a91e78aedefb565e0847dffbcbad4
Aaron Huang <chlbqj@gmail.com>
2019-09-14 18:46:49 +0800

    爬球員圖片改成分字母爬

--- python/playerPicCrawler.py
+++ python/playerPicCrawler.py
@@ -9,10 +9,11 @@ from selenium.webdriver.support import expected_conditions as EC
 URL = "https://stats.nba.com/players/list/?Historic=Y"
 
 class PlayerPictureCrawler:
-    def __init__(self, url):
+    def __init__(self, url, index):
         self.url_to_crawl = url
         self.result = []
         self.links = []
+        self.index = index
 
     def start_driver(self):
         print("啟動 WebDriver...")
@@ -29,16 +30,21 @@ class PlayerPictureCrawler:
         time.sleep(2)
 
     def save_to_json(self, result):
-        with open("./XIANG/player_pic.json", 'w') as file_object:
+        file_name = "./JSON/player_pic" + self.index + ".json"
+        with open(file_name, 'w') as file_object:
             json.dump(result, file_object)
+            print(file_name + "完成！！！")
 
-    def parse_link(self):
+    def parse_link(self, index):
         print("取得球員列表...")
         domain = "https://stats.nba.com"
         res = BS(self.driver.page_source, 'lxml')
-        list = res.select('.players-list__name')
+        temp_index = ord(index) - 65
+        section = res.select('.players-list__section')[temp_index]
+        list = section.select('.players-list__name')
         for player in list:
             link = player.select('a')[0]
+            # print(link.text)
             self.links.append(domain + link.get('href'))
 
     def parse_player(self):
@@ -74,23 +80,24 @@ class PlayerPictureCrawler:
 
     # 進入各個球員頁面
     def go_each_player(self):
-        i = 0
+        # i = 0
         for link in self.links:
             self.get_page(link)
             self.parse_player()
-            i += 1
-            if i == 15: #定時存檔
-                self.save_to_json(self.result)
-                print("存檔成功！")
-                i = 0
+            # i += 1
+            # if i == 15: #定時存檔
+            #     self.save_to_json(self.result)
+            #     print("存檔成功！")
+            #     i = 0
 
     def parse(self):
         self.start_driver()     # 開啟 WebDriver
         self.get_page(self.url_to_crawl)    # 進入球員index
-        self.parse_link()   # 取得所有球員url
+        self.parse_link(self.index)   # 取得所有球員url
         self.go_each_player()
         self.close_driver()     # 關閉 WebDriver
 
 if __name__ == '__main__':
-    crawler = PlayerPictureCrawler(URL)
+    index = input("請輸入爬蟲球員字母開頭(A~Z)，注意要大寫：")
+    crawler = PlayerPictureCrawler(URL, index)
     crawler.parse()
