3458655e2bd1d6016874960330a48dc239414826
Aaron Huang <chlbqj@gmail.com>
2019-09-30 01:20:14 +0800

    team_summary

--- python/Process/test.py
+++ python/Process/test.py
@@ -2,9 +2,32 @@ import json
 import pandas
 
 class Test:
-  def __init__(self):
-    
-  def openFile(self, name):
-        fileName = '../JSON/' + name
+    def __init__(self):
+        self.result = pandas.DataFrame()
+
+    # 開檔並讀取資料
+    def openFile(self, name):
+        fileName = '../JSON/' + name + '.json'
         with open(fileName, 'r') as loadFile:
-            return json.load(loadFile)
+            print('開啟' + fileName + '...')
+            return pandas.DataFrame(json.load(loadFile))
+
+    # 最後寫入檔案
+    def save_to_json(self, name, data):
+        file_name = "../JSON/" + name + ".json"
+        with open(file_name, 'w') as file_object:
+            data = data.to_dict()
+            json.dump(data, file_object)
+            print(file_name + "完成！！！")
+
+    def Process(self):
+        for year in range(2010, 2020):
+            file_name = 'MVP/final/MVP_' + str(year)
+            temp = self.openFile(file_name)
+            self.result = pandas.concat([self.result, temp])
+        # print(self.result)
+        self.save_to_json('MVP/final_mvp', self.result)
+
+if __name__ == '__main__':
+    test = Test()
+    test.Process()
